23f4062fc1a1f102aa1c70524dd14855becb0aea
Mouse <937279156@qq.com>
2017-09-03 14:38:25 +0800

    Added block searcher(Need Fix)

---
+++ src/main/java/team/unstudio/jblockly/component/skin/BlockSearcherSkin.java
@@ -0,0 +1,12 @@
+package team.unstudio.jblockly.component.skin;
+
+import javafx.scene.control.SkinBase;
+import team.unstudio.jblockly.component.BlockSearcher;
+
+public class BlockSearcherSkin extends SkinBase<BlockSearcher>{
+
+	protected BlockSearcherSkin(BlockSearcher control) {
+		super(control);
+	}
+
+}
--- src/main/java/team/unstudio/jblockly/demo/Main.java
+++ src/main/java/team/unstudio/jblockly/demo/Main.java
@@ -1,16 +1,15 @@
 package team.unstudio.jblockly.demo;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import team.unstudio.jblockly.component.BlockList;
+import team.unstudio.jblockly.component.BlockSearcher;
 import team.unstudio.jblockly.input.SlotType;
-import team.unstudio.jblockly.util.provider.SimpleBlockProvider;
+import team.unstudio.jblockly.provider.SimpleBlockProvider;
 import team.unstudio.jblockly.Block;
 import team.unstudio.jblockly.BlockWorkspace;
 import team.unstudio.jblockly.ConnectionType;
@@ -24,25 +23,26 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		SimpleBlockProvider main = new SimpleBlockProvider().setConnectionType(ConnectionType.NONE).setRegistyName("main")
-				.addLabel("main").addBlockSlot().addBlockSlot("branch",SlotType.BRANCH);
+				.addLabel("main").addBlockSlot().addBlockSlot("branch",SlotType.BRANCH).register();
 		
 		SimpleBlockProvider ifBlock = new SimpleBlockProvider().setConnectionType(ConnectionType.TOPANDBOTTOM).setRegistyName("if")
 				.addLabel("如果").addBlockSlot("if", SlotType.INSERT).addBlockSlot("branch", SlotType.BRANCH)
-				.addBlockSlot().addBlockSlot("next",SlotType.NEXT).setFill(Color.WHITE).setStroke(Color.RED);
+				.addBlockSlot().addBlockSlot("next",SlotType.NEXT).setFill(Color.WHITE).setStroke(Color.RED).register();
 		
-		SimpleBlockProvider end = new SimpleBlockProvider().setConnectionType(ConnectionType.TOP).setRegistyName("end").addLabel("返回").addBlockSlot();
+		SimpleBlockProvider end = new SimpleBlockProvider().setConnectionType(ConnectionType.TOP).setRegistyName("end")
+				.addLabel("返回").addBlockSlot().register();
 
 		SimpleBlockProvider string = new SimpleBlockProvider().setConnectionType(ConnectionType.LEFT).setRegistyName("string")
-				.addLabel("\"").addTextField("text").addLabel("\"").addBlockSlot();
+				.addLabel("\"").addTextField("text").addLabel("\"").addBlockSlot().register();
 		
 		SimpleBlockProvider getDalao = new SimpleBlockProvider().setConnectionType(ConnectionType.LEFT).setRegistyName("getDalao")
-				.addLabel("获取大佬").addBlockSlot(null, SlotType.INSERT);
+				.addLabel("获取大佬").addBlockSlot(null, SlotType.INSERT).register();
 		
 		SimpleBlockProvider variable = new SimpleBlockProvider().setConnectionType(ConnectionType.TOPANDBOTTOM).setRegistyName("variable")
-				.addLabel("变量").addTextField("name").addBlockSlot(null, SlotType.INSERT).addNextSlot();
+				.addLabel("变量").addTextField("name").addBlockSlot(null, SlotType.INSERT).addNextSlot().register();
 		
 		SimpleBlockProvider set = new SimpleBlockProvider().setConnectionType(ConnectionType.LEFT).setRegistyName("set")
-				.addLabel("=").addBlockSlot(null, SlotType.INSERT);
+				.addLabel("=").addBlockSlot(null, SlotType.INSERT).register();
 		
 		BlockList blockList = new BlockList();
 		blockList.providersProperty().addAll(main,ifBlock,end,string,getDalao,variable,set);
@@ -57,11 +57,19 @@ public class Main extends Application {
 		rectangle.setLayoutX(0);
 		rectangle.setLayoutY(0);
 		
+		BlockSearcher searcher = new BlockSearcher();
+		searcher.setVisible(false);
+		
 		BlockWorkspace workspace = new BlockWorkspace();
+		
 		workspace.getChildren().add(scrollPane2);
 		blockList.setWorkspace(workspace);
+		
 		workspace.getChildren().add(rectangle);
 		workspace.addBlock(block);
+		
+		workspace.getChildren().add(searcher);
+		searcher.setWorkspace(workspace);
 		
 		rectangle.widthProperty().bind(block.widthProperty());
 		rectangle.heightProperty().bind(block.heightProperty());
@@ -72,17 +80,6 @@ public class Main extends Application {
 		});
 		scrollPane.hvalueProperty().addListener((observable, oldValue, newValue)->System.out.println(newValue));
 		scrollPane.vvalueProperty().addListener((observable, oldValue, newValue)->System.out.println(newValue));
-		
-//		Block bmain = main.build();
-//		workspace.addBlock(main.build());
-		
-//		workspace.addBlock(ifBlock.build());
-//		workspace.addBlock(hookan.build());
-//		workspace.addBlock(getDalao.build());
-//		workspace.addBlock(dalao.build());
-//		workspace.addBlock(dalao.build());
-//		workspace.addBlock(set.build());
-//		workspace.addBlock(end.build());
 
 		Scene scene = new Scene(scrollPane);
 
